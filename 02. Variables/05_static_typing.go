23bf547e9c699ecc9cadedc63e2be6e3db38bfdb
Ruslan Aliyev <156642047+ruslanaliyevn@users.noreply.github.com>
2025-12-29 20:59:58 +0400

    Create 05_static_typing.go

---
+++ 02. Variables/05_static_typing.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+	var cupsQty int = 3
+
+	fmt.Println("Number of cups:", cupsQty)
+
+	var wasProcessed = true
+
+	fmt.Println("Order was processed:", wasProcessed)
+
+}
