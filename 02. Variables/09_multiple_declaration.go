1876e97be2fb00040e074ff814a90c322c9012bd
Ruslan Aliyev <156642047+ruslanaliyevn@users.noreply.github.com>
2025-12-29 22:03:57 +0400

    Create 09_multiple_declaration.go

---
+++ 02. Variables/09_multiple_declaration.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	var coffee, milk, sugar = 1, 2, 1
+
+	// var coffee = 1
+	// var milk = 2
+	// var milk = 1
+
+	
+	fmt.Println("Coffee:", coffee, "Milk:", milk, "Sugar:", sugar)
+
+}
