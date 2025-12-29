ad35ad4dd8ccafd5b6befccc59760242276ed28e
Ruslan Aliyev <156642047+ruslanaliyevn@users.noreply.github.com>
2025-12-29 21:26:59 +0400

    Create 07_zero_values.go

---
+++ 02. Variables/07_zero_values.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main() {
+	var totalOrderQty int  		// default value is 0
+	var customerName string		// default value is ""
+	var isOederReady bool		// default value is false
+
+	fmt.Println("Total Orders quantiry:", totalOrderQty)
+	fmt.Println("Customer Name:", customerName)
+	fmt.Println("Order is ready:", isOederReady)
+
+}
