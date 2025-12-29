c8505c764eaaae64c67f1771f4273ec6f8989f0c
Ruslan Aliyev <156642047+ruslanaliyevn@users.noreply.github.com>
2025-12-29 20:27:43 +0400

    Update 03_variables_types.go

--- 02. Variables/03_variables_types.go
+++ 02. Variables/03_variables_types.go
@@ -1,1 +1,19 @@
-03_variables_types.go
+package main
+
+import "fmt"
+func main() {
+
+	name := "Americano"
+	price := 2.50
+	ready := true
+	orderCount := 5
+	var stockCount int64 = 5000
+
+	fmt.Printf("Type of name is: %T\n", name)
+	fmt.Printf("Type of price is: %T\n", price)
+	fmt.Printf("Type of ready is: %T\n", ready)
+	fmt.Printf("Type of orderCount is: %T\n", orderCount)
+	fmt.Printf("Type of stockCount is: %T\n", stockCount)
+
+	
+}
